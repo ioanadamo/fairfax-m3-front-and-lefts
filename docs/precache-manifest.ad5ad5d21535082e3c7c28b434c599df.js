6c15eb6c500e6662d2ad8a01e308416ca2251812
Irene Fuente <ifuenteromero@gmail.com>
2019-05-13 23:53:12 +0200

    update docs folder

---
+++ docs/precache-manifest.ad5ad5d21535082e3c7c28b434c599df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13c25c54ede6bcbac0486615cf463aa5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d185160849036bdf1257",
+    "url": "./static/css/main.b13c14fe.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "./static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "d185160849036bdf1257",
+    "url": "./static/js/main.4ffb041b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/logo-awesome-profile-cards.0118e311.svg"
+  },
+  {
+    "revision": "b11f55df99771bee2c66b5ba7ba6935c",
+    "url": "./static/media/logo-disena-aventura.b11f55df.jpg"
+  },
+  {
+    "revision": "35d7b1d393df651e46c9304b630cf73d",
+    "url": "./static/media/user-photo-default.35d7b1d3.png"
+  }
+]);
