6ad13879952cd336b245d39d2aedd16c9ba38dad
daniela932910 <daniela932910@gmail.com>
2019-05-16 01:47:57 +0200

    add component

--- src/components/PreviewSection.js
+++ src/components/PreviewSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import icoTrash from '../images/trash.svg';
-import PreviewName from './PreviewName';
-import PreviewJob from './PreviewJob';
+import PreviewCard from './PreviewCard';
 
 class PreviewSection extends React.Component {
 	render() {
@@ -12,52 +11,7 @@ class PreviewSection extends React.Component {
 						<img className="trash-icon" src={icoTrash} alt="imagen reset" />
 						Reset
 					</button>
-					<div className="preview__card">
-						<div className="deco-rectangle__card" />
-						<PreviewName nameSurname="Nombre Apellidos" />
-
-						<PreviewJob job="Front-end Developer" />
-
-						<div className="photo__preview js__profile-image" />
-
-						<ul className="social__card--list">
-							<li className="social__icons mobile-icon">
-								{' '}
-								<a href="" id="mobile-link">
-									<svg className="wrap-icon">
-										<use href="#ico-mobile" />
-									</svg>
-								</a>
-							</li>
-
-							<li className="social__icons email-icon">
-								{' '}
-								<a className="email__card">
-									<svg className="wrap-icon">
-										<use href="#ico-letter" />
-									</svg>
-								</a>
-							</li>
-
-							<li className="social__icons linkedin-icon">
-								{' '}
-								<a className="linkedin-link" href="" target="_blank">
-									<svg className="wrap-icon">
-										<use href="#ico-linkedin" />
-									</svg>
-								</a>
-							</li>
-
-							<li className="social__icons github-icon">
-								{' '}
-								<a className="link-github" href="" target="_blank">
-									<svg className="wrap-icon">
-										<use href="#ico-github" />
-									</svg>
-								</a>
-							</li>
-						</ul>
-					</div>
+					<PreviewCard />
 				</div>
 			</section>
 		);
