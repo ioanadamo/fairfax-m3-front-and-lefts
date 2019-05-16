8edff23918cd4e1ed81f8d96dcb7fa97cdccd810
daniela932910 <daniela932910@gmail.com>
2019-05-16 12:41:52 +0200

    add preview card file

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import logoAdalab from './images/logo-adalab.png';
 import logoAwesome from './images/logo-awesome-profile-cards.svg';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Svgs from "./components/Svgs";
+import Svgs from './components/Svgs';
 import PreviewSection from './components/PreviewSection';
 import FormSection from './components/FormSection';
 import './App.css';
 
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-function App() {
-  return (
-    <div className="place-items__cardmaker">
-      <Svgs />
-      <Header link="index.html" logoSrc={logoAwesome} />
+	render() {
+		return (
+			<div className="place-items__cardmaker">
+				<Svgs />
+				<Header link="index.html" logoSrc={logoAwesome} />
 
-			<main className="main-content column-center">
-				<PreviewSection />
-        <FormSection />
-			</main>
-      <Footer firstLogo={logoDisena} secondLogo={logoAdalab} />
-    </div>
-  );
+				<main className="main-content column-center">
+					<PreviewSection />
+					<FormSection />
+				</main>
+				<Footer firstLogo={logoDisena} secondLogo={logoAdalab} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/PreviewCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PreviewName from './PreviewName';
+import PreviewJob from './PreviewJob';
+
+class PreviewCard extends React.Component {
+	render() {
+		return (
+			<div className="preview__card">
+				<div className="deco-rectangle__card" />
+				<PreviewName nameSurname="Nombre Apellidos" />
+
+				<PreviewJob job="Front-end Developer" />
+
+				<div className="photo__preview js__profile-image" />
+
+				<ul className="social__card--list">
+					<li className="social__icons mobile-icon">
+						{' '}
+						<a href="" id="mobile-link">
+							<svg className="wrap-icon">
+								<use href="#ico-mobile" />
+							</svg>
+						</a>
+					</li>
+
+					<li className="social__icons email-icon">
+						{' '}
+						<a className="email__card">
+							<svg className="wrap-icon">
+								<use href="#ico-letter" />
+							</svg>
+						</a>
+					</li>
+
+					<li className="social__icons linkedin-icon">
+						{' '}
+						<a className="linkedin-link" href="" target="_blank">
+							<svg className="wrap-icon">
+								<use href="#ico-linkedin" />
+							</svg>
+						</a>
+					</li>
+
+					<li className="social__icons github-icon">
+						{' '}
+						<a className="link-github" href="" target="_blank">
+							<svg className="wrap-icon">
+								<use href="#ico-github" />
+							</svg>
+						</a>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default PreviewCard;
