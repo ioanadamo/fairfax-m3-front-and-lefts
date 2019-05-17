bab3fd0b35f543a35d4889ffb4b374b54bf0b14e
Irene Fuente <ifuenteromero@gmail.com>
2019-05-17 13:27:12 +0200

    Clean code

--- src/components/Palettes.js
+++ src/components/Palettes.js
@@ -1,99 +1,81 @@
 import React from 'react';
-import { thisTypeAnnotation } from '@babel/types';
 
-class Palettes extends React.Component{
-    constructor(props){
+class Palettes extends React.Component {
+    constructor(props) {
         super(props)
         this.handleClick = this.props.actionPerform;
-        
     }
-    
-    render(){
-        
-        return(
 
+    render() {
+        return (
             <div className="panel__design-radios panel--openFlex">
-            <p className="radios__title">Colores</p>
-            <div className="radios__picker">
-                <label
-                    className="control__text radios-text"
-                    htmlFor="option-color-1"
-                >
-                    <input
-                        className="control__radio radios-buttons"
-                        id="option-color-1"
-                        type="radio"
-                        value="1"
-                        name="palette"
-                        checked={this.props.paletteNumber==1}
-                       
-                        onClick = {this.handleClick}
-                    />{' '}
-                    <span className="control__indicator" />
-                    <span className="box-wrapper">
-                        <span className="box box-color1" />
-                        <span className="box box-color2" />
-
-                        <span className="box box-color3" />
-                    </span>
-                </label>
-                <label
-                    className="control__text radios-text"
-                    htmlFor="option-color-2"
-                >
-                    <input
-                        className="control__radio radios-buttons"
-                        id="option-color-2"
-                        type="radio"
-                        value="2"
-                        name="palette"
-                        checked={this.props.paletteNumber==2}
-                        onClick = {this.handleClick}
-                    />{' '}
-                    <span className="control__indicator" />
-                    <span className="box-wrapper">
-                        <span className="box box-color4" />
-                        <span className="box box-color5" />
-
-                        <span className="box box-color6" />
-                    </span>
-                </label>
-                <label
-                    className="control__text radios-text"
-                    htmlFor="option-color-3"
-                >
-                    <input
-                        className="control__radio radios-buttons"
-                        id="option-color-3"
-                        type="radio"
-                        value="3"
-                        name="palette"
-                        checked={this.props.paletteNumber==3}
-                        onClick = {this.handleClick}
-                    />
-                    <span className="control__indicator" />
-                    <span className="box-wrapper">
-                        <span className="box box-color7" />
-                        <span className="box box-color8" />
-
-                        <span className="box box-color9" />
-                    </span>
-                </label>
+                <p className="radios__title">Colores</p>
+                <div className="radios__picker">
+                    <label
+                        className="control__text radios-text"
+                        htmlFor="option-color-1"
+                    >
+                        <input
+                            className="control__radio radios-buttons"
+                            id="option-color-1"
+                            type="radio"
+                            value="1"
+                            name="palette"
+                            checked={this.props.paletteNumber == 1}
+                            onClick={this.handleClick}
+                        />
+                        <span className="control__indicator" />
+                        <span className="box-wrapper">
+                            <span className="box box-color1" />
+                            <span className="box box-color2" />
+                            <span className="box box-color3" />
+                        </span>
+                    </label>
+                    <label
+                        className="control__text radios-text"
+                        htmlFor="option-color-2"
+                    >
+                        <input
+                            className="control__radio radios-buttons"
+                            id="option-color-2"
+                            type="radio"
+                            value="2"
+                            name="palette"
+                            checked={this.props.paletteNumber == 2}
+                            onClick={this.handleClick}
+                        />
+                        <span className="control__indicator" />
+                        <span className="box-wrapper">
+                            <span className="box box-color4" />
+                            <span className="box box-color5" />
+                            <span className="box box-color6" />
+                        </span>
+                    </label>
+                    <label
+                        className="control__text radios-text"
+                        htmlFor="option-color-3"
+                    >
+                        <input
+                            className="control__radio radios-buttons"
+                            id="option-color-3"
+                            type="radio"
+                            value="3"
+                            name="palette"
+                            checked={this.props.paletteNumber == 3}
+                            onClick={this.handleClick}
+                        />
+                        <span className="control__indicator" />
+                        <span className="box-wrapper">
+                            <span className="box box-color7" />
+                            <span className="box box-color8" />
+                            <span className="box box-color9" />
+                        </span>
+                    </label>
+                </div>
             </div>
-        </div>
-
-
         );
     }
 
-
-
-
-
 }
-
-
-
-
 
 export default Palettes;
